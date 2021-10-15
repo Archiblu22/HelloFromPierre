c8e61ea266487d3fd66b04de5e1264ebaae156a7
MusicalMonarch <alyssalahoy@gmail.com>
2021-10-15 16:47:22 -0700

    Replied to Pierre's hello - Alyssa

--- src/aguirre/Main.java
+++ src/aguirre/Main.java
@@ -3,6 +3,7 @@ package aguirre;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("\nHello, my name is Pierre.");
+        System.out.println("\nHello, my name is Pierre.");
+        System.out.println("\nHello, Pierre, my name is Alyssa.");
     }
 }
